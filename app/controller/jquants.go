6eb8c0b36badc0167e577776e9dec1f4ec9229cb
root-5 <hujisannrokuoumunaku.shigoto@gmail.com>
2024-07-13 17:19:24 +0900

    DB接続とgorm導入

--- app/controller/jquants.go
+++ app/controller/jquants.go
@@ -12,18 +12,17 @@ import (
 	"strings"
 )
 
+// HTTPクライアント
 var httpClient = &http.Client{}
 
 /*
-POSTリクエストを行い、レスポンスボディを取得する関数
-
+GET, POSTリクエストを行い、レスポンスボディを取得する関数
 	- 入力) url - リクエスト先URL文字列
 	- 入力) queryParam - クエリパラメータ構造体
 	- 入力) reqBody - リクエストボディ構造体
 	- 入力) resBody - レスポンスボディ構造体のポインタ
 	- 出力) err - エラー
 */
-// post: POSTリクエストを行い、レスポンスボディを取得
 func post[T any](reqUrl string, queryParams any, reqBody any, resBody *T) (err error) {
 	// クエリパラメータをreqURLに追加
 	if queryParams != struct{}{} {
@@ -78,14 +77,13 @@ func post[T any](reqUrl string, queryParams any, reqBody any, resBody *T) (err e
 
 /*
 JQuants に登録したメールアドレスとパスワードを入力して、リフレッシュトークン（期限: 1週間）を取得する関数
-
 	- 入力) email - JQuant に登録したメールアドレス
 	- 入力) pass - JQuant に登録したパスワード
 	- 出力) refreshToken - リフレッシュトークン
 	- 出力) err - エラー
 */
-func GetRefreshToken(email string, pass string) (refreshToken string, err error) {
-	return "eyJjdHkiOiJKV1QiLCJlbmMiOiJBMjU2R0NNIiwiYWxnIjoiUlNBLU9BRVAifQ.lEvoH4XOE-grkUGtMLDwvuhiDtkIHEG4k2COyW9UtTqcxjeRKRVNmJqQZ2jb0WqNXcmUcEwAV6T6katBLZseg8Z2IkE0_-J2_BW1NRRYl_PbuXSH42SbuQgzTAtduLnMSliK17kGBSyvG_e0cd6GoivG0NRpmry9GUHgdYqvq_i0VYr2noWai1JOB3Is0_O2cJHPrSV3CdjH22X7u07qZWAQJekj1KioUAAQJb0Igu0I0-HiY2fO9cM-5tkobIJCdNy9zY-1iFKD3MtaXoCXqyXn6wpa-ao3ErfKBIwqV_b2OmuoiVbpT5Ab3fawcmq4bLcOMihOZJZ-oMVClmEriA.UrkYH_mZjHiBRE37.4uhGdOjUFDAahYCoD2XLf5JNuNZz7sLCMVMYOIGC4WJn5Eu88Pw7QP7tboeeqoKXv4FUoQ5Tve1mdvD3OwWfe8hlb0kXYYjqxaVSFlVPF2w0JJzHToiWRNBoRmzaNJe1ImJ3p8dVFrN7d7w5kyDxfKFTdnxvuYLZJMqmYdMUMn8M4ALKU-MTP9BxdA42qiCiomxAVLbX2zhM7VNC1Y_sXWnnSG9Lw9pUkWEoLV0ccJLyraRIa5oUSQ9WIuf-kuVb3Bu6KfwPFnl4lcEyaDoXObXd9b2xzTxmNm-CjAwHxFX5S577xaXR2oDEoQjpZjFFvjIM1ISBLFCVvx7zT6aB1Olpt-OnqaR2kumOy5jb7126R79TgdIsdyMbpNIDHxBtTXFhsFaI2bp5i9Uko3hC_TZdofyxmreYChHYPcCO1PO-j_857JnH5W_qExm3_HqQl5Io9ZmUaNW5UDfbzeCN2hp3-FAQ3fU41-kp1Vt1manP0-JAUagtDUtKc76j2S94JbZiSZiWEVKVI_cUZlfeePvok4R5g4qJslXSpql5vA27Hz_LoD8LOf1J_CvnylLDWStrOgth0QBz3mWJeH2G9n1Qww-SOd8bsKPtBXtqGroO5LfE9nTNPr1QFGDryNEjR653d1Sq-76N4oaRIZ9pv-JlclY0_0hNH6NkVgpZcWDEt4BL8wkEaZNJJSBuCHC6tX6W87_Ece6rkq9XYT-HQajNEF_eSePlAhw6RmjIGpUDdlf4UtOQNCnrqNfokca21zS6uM-2SGq0I-nQFtO-rkmjVEgXE8oXszNHG3P63yvPbof1YGK7Qz0cI27FSNUnIR42NpX0ft6U-KZoO96uJmf49zhS_dRUgv_8y7jaa-9zmwhCg-mC9ZLnn6FFWS4ONNKGCCdTNDhy1VEpDI7okX37Q56j61pebe6rhuLFDHeEUpEOBOwH8JhmwP7niTa6xbFm7HjooTnRlhY0SltMoulFAydgzCQNCIwKtsOMGqqqu7EJTMIxmeXQb8z4AGO-VD9vM9o3OUzN-ohnO2VTa3n_pAJOQf6x4kiDPfyyvTCUhRY3GL4ML-nwyntNVrC1wlHB1MiQoQjdrKCHA6Ppy2B07wYA4e9jqaPNZYXiiBh9q6XB1jojc9OPuHZJvKV64TQPIP9RRDt8GMYDv0jC2Wz7jKTbp_boim9DQTIgRaFsa3miSjYL6ikQOMW_Viq9Nm0WI32t2mTOQQ2gREhdWNLuks6YaXoaNI17_NYjp6Bl4NpaDD_-GOdeJiXtKgF64ZCAJJadGolkN5aQPsfT4oMsRLUwCX4S6f9b-dR0jH4ppTqz95XxT94uU2hN8E48GhelP4YIZlRqrw.WyO9VqhsLA4Og65fcm2VhA", nil
+func getRefreshToken(email string, pass string) (refreshToken string, err error) {
+	// return "eyJjdHkiOiJKV1QiLCJlbmMiOiJBMjU2R0NNIiwiYWxnIjoiUlNBLU9BRVAifQ.lEvoH4XOE-grkUGtMLDwvuhiDtkIHEG4k2COyW9UtTqcxjeRKRVNmJqQZ2jb0WqNXcmUcEwAV6T6katBLZseg8Z2IkE0_-J2_BW1NRRYl_PbuXSH42SbuQgzTAtduLnMSliK17kGBSyvG_e0cd6GoivG0NRpmry9GUHgdYqvq_i0VYr2noWai1JOB3Is0_O2cJHPrSV3CdjH22X7u07qZWAQJekj1KioUAAQJb0Igu0I0-HiY2fO9cM-5tkobIJCdNy9zY-1iFKD3MtaXoCXqyXn6wpa-ao3ErfKBIwqV_b2OmuoiVbpT5Ab3fawcmq4bLcOMihOZJZ-oMVClmEriA.UrkYH_mZjHiBRE37.4uhGdOjUFDAahYCoD2XLf5JNuNZz7sLCMVMYOIGC4WJn5Eu88Pw7QP7tboeeqoKXv4FUoQ5Tve1mdvD3OwWfe8hlb0kXYYjqxaVSFlVPF2w0JJzHToiWRNBoRmzaNJe1ImJ3p8dVFrN7d7w5kyDxfKFTdnxvuYLZJMqmYdMUMn8M4ALKU-MTP9BxdA42qiCiomxAVLbX2zhM7VNC1Y_sXWnnSG9Lw9pUkWEoLV0ccJLyraRIa5oUSQ9WIuf-kuVb3Bu6KfwPFnl4lcEyaDoXObXd9b2xzTxmNm-CjAwHxFX5S577xaXR2oDEoQjpZjFFvjIM1ISBLFCVvx7zT6aB1Olpt-OnqaR2kumOy5jb7126R79TgdIsdyMbpNIDHxBtTXFhsFaI2bp5i9Uko3hC_TZdofyxmreYChHYPcCO1PO-j_857JnH5W_qExm3_HqQl5Io9ZmUaNW5UDfbzeCN2hp3-FAQ3fU41-kp1Vt1manP0-JAUagtDUtKc76j2S94JbZiSZiWEVKVI_cUZlfeePvok4R5g4qJslXSpql5vA27Hz_LoD8LOf1J_CvnylLDWStrOgth0QBz3mWJeH2G9n1Qww-SOd8bsKPtBXtqGroO5LfE9nTNPr1QFGDryNEjR653d1Sq-76N4oaRIZ9pv-JlclY0_0hNH6NkVgpZcWDEt4BL8wkEaZNJJSBuCHC6tX6W87_Ece6rkq9XYT-HQajNEF_eSePlAhw6RmjIGpUDdlf4UtOQNCnrqNfokca21zS6uM-2SGq0I-nQFtO-rkmjVEgXE8oXszNHG3P63yvPbof1YGK7Qz0cI27FSNUnIR42NpX0ft6U-KZoO96uJmf49zhS_dRUgv_8y7jaa-9zmwhCg-mC9ZLnn6FFWS4ONNKGCCdTNDhy1VEpDI7okX37Q56j61pebe6rhuLFDHeEUpEOBOwH8JhmwP7niTa6xbFm7HjooTnRlhY0SltMoulFAydgzCQNCIwKtsOMGqqqu7EJTMIxmeXQb8z4AGO-VD9vM9o3OUzN-ohnO2VTa3n_pAJOQf6x4kiDPfyyvTCUhRY3GL4ML-nwyntNVrC1wlHB1MiQoQjdrKCHA6Ppy2B07wYA4e9jqaPNZYXiiBh9q6XB1jojc9OPuHZJvKV64TQPIP9RRDt8GMYDv0jC2Wz7jKTbp_boim9DQTIgRaFsa3miSjYL6ikQOMW_Viq9Nm0WI32t2mTOQQ2gREhdWNLuks6YaXoaNI17_NYjp6Bl4NpaDD_-GOdeJiXtKgF64ZCAJJadGolkN5aQPsfT4oMsRLUwCX4S6f9b-dR0jH4ppTqz95XxT94uU2hN8E48GhelP4YIZlRqrw.WyO9VqhsLA4Og65fcm2VhA", nil
 	// return "dummy_refreshtoken", nil
 
 	// リクエスト先URL
@@ -125,13 +123,12 @@ func GetRefreshToken(email string, pass string) (refreshToken string, err error)
 
 /*
 リフレッシュトークンを渡して、ID トークン（期限: 24時間）を取得する関数
-
-	- 入力) refreshToken - GetRefreshToken 関数で取得したトークン
+	- 入力) refreshToken - getRefreshToken 関数で取得したトークン
 	- 出力) idToken - ID トークン
 	- 出力) err - エラー
 */
-func GetIdToken(refreshToken string) (idToken string, err error) {
-	return "eyJraWQiOiJHQXNvU2xxUzMyUktLT2lVYm1xcjU3ekdYNE1TVFhsWFBrbDNJTmhWKzNzPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI3ZWVjNGQ3Ni03NzhmLTQ1NzAtOGY2Ni0zNjRkZjc0MWZkNmIiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLmFwLW5vcnRoZWFzdC0xLmFtYXpvbmF3cy5jb21cL2FwLW5vcnRoZWFzdC0xX0FGNjByeXJ2NCIsImNvZ25pdG86dXNlcm5hbWUiOiI3ZWVjNGQ3Ni03NzhmLTQ1NzAtOGY2Ni0zNjRkZjc0MWZkNmIiLCJvcmlnaW5fanRpIjoiNWRlZTcxODgtYzhhNy00Njk3LWJiZGMtN2VmYmFkNTAwYTVmIiwiYXVkIjoiNXZyN2xiOGppdThvZmhvZmJmYWxmbm1waWkiLCJldmVudF9pZCI6Ijk4Njk1MWM2LTQxNGQtNDIzNy04Nzk4LWZmY2EwZjE0ODJiYiIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNzE4NTQwNTM4LCJleHAiOjE3MTg2MjY5NzMsImlhdCI6MTcxODU0MDU3MywianRpIjoiODI2ZTQ2ZGItYTRmZC00Y2M0LTkzNjUtMjdkZTNmNDhiMTA1IiwiZW1haWwiOiJ3YXRlci52aWxsYWdlLnJpZ2h0Lm5lYXJAZ21haWwuY29tIn0.Wv00zXyDqhGetvei6Uet38bxIM87RgyTZTUeh7chgTHuya1FgfWeF-QUdIO6CjiKz87TZdTPC7hhzD6VcJMzRMUv3J7cf6RYcBSiEd1siLJEhtd06qzdROwLOURJSclFKO9HQo0RhU_YmUUj-sWyFJVbtrvYesqQN2jR0dDcQYJs3ITiS3QDl9sQfZvKAXVPJOTU_rm-mCYBpdL1iJAxRNCO2dwqqMTx9rDyplf383ndV4WaUwB9dJefj-LRJdn_r052muQQ68PJfispvOgkqr9GGHtilgcZnXBSN2vn2L0EowqE7cRWHf4DPXM8PAm3WL2E8BiuvNKD_3XZNoFOEA", nil
+func getIdToken(refreshToken string) (idToken string, err error) {
+	// return "eyJraWQiOiJHQXNvU2xxUzMyUktLT2lVYm1xcjU3ekdYNE1TVFhsWFBrbDNJTmhWKzNzPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI3ZWVjNGQ3Ni03NzhmLTQ1NzAtOGY2Ni0zNjRkZjc0MWZkNmIiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLmFwLW5vcnRoZWFzdC0xLmFtYXpvbmF3cy5jb21cL2FwLW5vcnRoZWFzdC0xX0FGNjByeXJ2NCIsImNvZ25pdG86dXNlcm5hbWUiOiI3ZWVjNGQ3Ni03NzhmLTQ1NzAtOGY2Ni0zNjRkZjc0MWZkNmIiLCJvcmlnaW5fanRpIjoiNWRlZTcxODgtYzhhNy00Njk3LWJiZGMtN2VmYmFkNTAwYTVmIiwiYXVkIjoiNXZyN2xiOGppdThvZmhvZmJmYWxmbm1waWkiLCJldmVudF9pZCI6Ijk4Njk1MWM2LTQxNGQtNDIzNy04Nzk4LWZmY2EwZjE0ODJiYiIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNzE4NTQwNTM4LCJleHAiOjE3MTg2MjY5NzMsImlhdCI6MTcxODU0MDU3MywianRpIjoiODI2ZTQ2ZGItYTRmZC00Y2M0LTkzNjUtMjdkZTNmNDhiMTA1IiwiZW1haWwiOiJ3YXRlci52aWxsYWdlLnJpZ2h0Lm5lYXJAZ21haWwuY29tIn0.Wv00zXyDqhGetvei6Uet38bxIM87RgyTZTUeh7chgTHuya1FgfWeF-QUdIO6CjiKz87TZdTPC7hhzD6VcJMzRMUv3J7cf6RYcBSiEd1siLJEhtd06qzdROwLOURJSclFKO9HQo0RhU_YmUUj-sWyFJVbtrvYesqQN2jR0dDcQYJs3ITiS3QDl9sQfZvKAXVPJOTU_rm-mCYBpdL1iJAxRNCO2dwqqMTx9rDyplf383ndV4WaUwB9dJefj-LRJdn_r052muQQ68PJfispvOgkqr9GGHtilgcZnXBSN2vn2L0EowqE7cRWHf4DPXM8PAm3WL2E8BiuvNKD_3XZNoFOEA", nil
 	// return "dummy_idtoken", nil
 
 	// リクエスト先URL
@@ -163,6 +160,31 @@ func GetIdToken(refreshToken string) (idToken string, err error) {
 
 	// トークンを取得
 	idToken = resBody.IdToken
+
+	return idToken, nil
+}
+
+/*
+メールアドレスとパスワードを入力して、ID トークン（期限: 24時間）を取得する関数
+	- 入力) email - JQuant に登録したメールアドレス
+	- 入力) pass - JQuant に登録したパスワード
+	- 出力) idToken - ID トークン
+	- 出力) err - エラー
+*/
+func SetIdToken(email string, pass string) (idToken string, err error) {
+	return "aaa", nil
+
+	// リフレッシュトークンを取得
+	refreshToken, err := getRefreshToken(email, pass)
+	if err != nil {
+		return "", fmt.Errorf("getRefreshToken Error: %v", err)
+	}
+
+	// ID トークンを取得
+	idToken, err = getIdToken(refreshToken)
+	if err != nil {
+		return "", fmt.Errorf("getIdToken Error: %v", err)
+	}
 
 	return idToken, nil
 }
--- app/main.go
+++ app/main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
-	"app/controller"
+	jquants "app/controller"
+
 	"fmt"
 	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+  }
 
 func main() {
 	fmt.Println("Program started")
@@ -13,22 +23,55 @@ func main() {
 	email := os.Getenv("JQUANTS_EMAIL")
 	pass := os.Getenv("JQUANTS_PASS")
 
-	// メールアドレスとパスワードを渡してリフレッシュトークンを取得
-	refreshToken, err := jquants.GetRefreshToken(email, pass)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	// fmt.Println("RefreshToken: ", refreshToken)
 
-	// リフレッシュトークンを渡して ID トークンを取得
-	idToken, err := jquants.GetIdToken(refreshToken)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	// fmt.Println("IdToken: ", idToken)
+
+	// DB の初期化
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DB")
+
+	dsn := "host=" + host + " user=" + user + " password=" + password + " dbname=" + dbname + " port=" + port + " sslmode=disable TimeZone=Asia/Tokyo"
+	fmt.Println(dsn)
+
 	
-	_ = idToken
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+	  panic("failed to connect database")
+	}
+  
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
+  
+	// Create
+	db.Create(&Product{Code: "D42", Price: 100})
+  
+	// Read
+	var product Product
+	db.First(&product, 1) // find product with integer primary key
+	db.First(&product, "code = ?", "D42") // find product with code D42
+  
+	// Update - update product's price to 200
+	db.Model(&product).Update("Price", 200)
+	// Update - update multiple fields
+	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
+	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+  
+	// Delete - delete product
+	// db.Delete(&product, 1)
 
+	// Read
+	var productAlfa Product
+	db.First(&productAlfa, 1) // find product with integer primary key
+	fmt.Printf("ProductCode: %v\n", productAlfa.Code)
+	fmt.Printf("ProductPrice: %v\n", productAlfa.Price)
+
+	// ID トークンをセット
+	idToken, err := jquants.SetIdToken(email, pass)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_ = idToken
 }
