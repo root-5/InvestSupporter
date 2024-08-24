65669bdefd96ee03b13aef4cbd34b7b7800eddfd
root-5 <hujisannrokuoumunaku.shigoto@gmail.com>
2024-08-25 00:39:57 +0900

    Jquantsテストに /infra から環境変数を拾う処理追加

--- app/controller/jquants/jquants_test.go
+++ app/controller/jquants/jquants_test.go
@@ -4,13 +4,44 @@ package jquants_test
 import (
 	jquants "app/controller/jquants"
 	"fmt"
+	"os"
+	"strings"
 	"testing"
 )
 
 // Test は JQuants API 用のテスト関数
 func TestJQuants(t *testing.T) {
+	fmt.Println("==============================")
 	fmt.Println("Exec jquants.Test")
+	fmt.Println("==============================")
+
 	var err error
+
+	// .env ファイルを読み込む
+	envText, err := os.Open("../../../infra/app/.env")
+	if err != nil {
+		t.Errorf("Open .env failed: %v", err)
+		return
+	}
+	defer envText.Close()
+
+	// envText を1行ずつ読んで環境変数にセット
+	for {
+		var envLine string
+		_, err := fmt.Fscanln(envText, &envLine)
+		if err != nil {
+			break
+		}
+		// envLine を "=" で分割、「"」が含まれている場合は削除してから環境変数にセット
+		envLineSlice := strings.Split(envLine, "=")
+		envLineSlice[1] = strings.Replace(envLineSlice[1], "\"", "", -1)
+		os.Setenv(envLineSlice[0], envLineSlice[1])
+	}
+	// 環境変数がセットされているか確認
+	if os.Getenv("JQUANTS_EMAIL") == "" || os.Getenv("JQUANTS_PASS") == "" {
+		t.Errorf("Set env failed: %v", err)
+		return
+	}
 
 	/*
 		Init
