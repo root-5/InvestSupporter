72056a32eefb0322d3052766c04e70f86766e268
root-5 <hujisannrokuoumunaku.shigoto@gmail.com>
2024-08-29 22:31:15 +0900

    データ挿入もSQL内で行うように修正

--- infra/db/docker-entrypoint-initdb.d/initial.sql
+++ infra/db/docker-entrypoint-initdb.d/initial.sql
@@ -85,7 +85,74 @@ CREATE INDEX idx_code_stocks_info ON stocks_info (code);
 CREATE INDEX idx_code_financial_info ON financial_info (code);
 CREATE INDEX idx_code_price_info ON price_info (code);
 
--- csv ファイルの読み込み
-COPY sector17_info FROM '/docker-entrypoint-initdb.d/sector17_info.csv' WITH (FORMAT csv, HEADER true);
-COPY sector33_info FROM '/docker-entrypoint-initdb.d/sector33_info.csv' WITH (FORMAT csv, HEADER true);
-COPY market_info FROM '/docker-entrypoint-initdb.d/market_info.csv' WITH (FORMAT csv, HEADER true);
+-- データの挿入
+-- 17 業種情報テーブル
+INSERT INTO sector17_info (sector17_code, sector17_name) VALUES
+    (1,'食品'),
+    (2,'エネルギー資源'),
+    (3,'建設・資材'),
+    (4,'素材・化学'),
+    (5,'医薬品'),
+    (6,'自動車・輸送機'),
+    (7,'鉄鋼・非鉄'),
+    (8,'機械'),
+    (9,'電機・精密'),
+    (10,'情報通信・サービスその他'),
+    (11,'電気・ガス'),
+    (12,'運輸・物流'),
+    (13,'商社・卸売'),
+    (14,'小売'),
+    (15,'銀行'),
+    (16,'金融（除く銀行）'),
+    (17,'不動産'),
+    (99,'その他');
+
+-- 33 業種情報テーブル
+INSERT INTO sector33_info (sector33_code, sector33_name) VALUES
+    (50,'水産・農林業'),
+    (1050,'鉱業'),
+    (2050,'建設業'),
+    (3050,'食料品'),
+    (3100,'繊維製品'),
+    (3150,'パルプ・紙'),
+    (3200,'化学'),
+    (3250,'医薬品'),
+    (3300,'石油･石炭製品'),
+    (3350,'ゴム製品'),
+    (3400,'ガラス･土石製品'),
+    (3450,'鉄鋼'),
+    (3500,'非鉄金属'),
+    (3550,'金属製品'),
+    (3600,'機械'),
+    (3650,'電気機器'),
+    (3700,'輸送用機器'),
+    (3750,'精密機器'),
+    (3800,'その他製品'),
+    (4050,'電気･ガス業'),
+    (5050,'陸運業'),
+    (5100,'海運業'),
+    (5150,'空運業'),
+    (5200,'倉庫･運輸関連業'),
+    (5250,'情報･通信業'),
+    (6050,'卸売業'),
+    (6100,'小売業'),
+    (7050,'銀行業'),
+    (7100,'証券･商品先物取引業'),
+    (7150,'保険業'),
+    (7200,'その他金融業'),
+    (8050,'不動産業'),
+    (9050,'サービス業'),
+    (9999,'その他');
+
+-- 市場区分情報テーブル
+INSERT INTO market_info (market_code, market_name) VALUES
+    (101,'東証一部'),
+    (102,'東証二部'),
+    (104,'マザーズ'),
+    (105,'TOKYO PRO MARKET'),
+    (106,'JASDAQ スタンダード'),
+    (107,'JASDAQ グロース'),
+    (109,'その他'),
+    (111,'プライム'),
+    (112,'スタンダード'),
+    (113,'グロース');
