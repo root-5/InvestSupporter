54154bee7c02eeeba3b71a59c2b4b8efd9c2d8b7
root-5 <hujisannrokuoumunaku.shigoto@gmail.com>
2024-09-11 07:07:24 +0900

    ログ出力を見やすく

--- app/controller/log/log.go
+++ app/controller/log/log.go
@@ -8,18 +8,13 @@ import (
 )
 
 func Info(msg string) {
-	fmt.Printf("\x1b[34m%s\x1b[0m	%s\n", time.Now().Format("2006-01-02 15:04:05"), msg)
+	fmt.Printf("\x1b[48;5;520m%s\x1b[0m	%s\n", time.Now().Format("2006-01-02 15:04:05"), msg)
 }
 
 func Error(err error) {
 	pc, file, line, ok := runtime.Caller(1)
 	if ok {
-		fmt.Printf(`
-======================== Error ============================
-%s	%s/%d %s
-			%v
-============================================================
-`,
+		fmt.Printf("\x1b[48;5;520m%s\x1b[0m	\x1b[48;5;160m ERROR \x1b[0m\n			%s/%d  %s\n			%v\n",
 			time.Now().Format("2006-01-02 15:04:05"),
 			filepath.Base(file),
 			line,
