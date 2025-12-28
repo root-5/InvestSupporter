76465a7b8eca8319162cb04de056a798bd988fe4
root-5 <hujisannrokuoumunaku.shigoto@gmail.com>
2025-12-29 03:18:43 +0900

    分かり易い点だけ修正

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,34 +1,34 @@
 variable "project_id" {
-  description = "GCP Project ID"
+  description = "プロジェクト ID"
   type        = string
 }
 
 variable "region" {
-  description = "GCP Region"
+  description = "リージョン"
   type        = string
   default     = "asia-northeast1"
 }
 
 variable "zone" {
-  description = "GCP Zone"
+  description = "ゾーン"
   type        = string
   default     = "asia-northeast1-a"
 }
 
 variable "machine_type" {
-  description = "GCE Machine Type"
+  description = CE タイプ"
   type        = string
-  default     = "e2-medium"
+  default     = "e2-micro"
 }
 
 variable "instance_name" {
-  description = "GCE Instance Name"
+  description = "インスタンス名"
   type        = string
   default     = "invest-supporter-app"
 }
 
 variable "allowed_ssh_ips" {
-  description = "List of IP addresses allowed to SSH"
+  description = "SSH 接続を許可する IP リスト"
   type        = list(string)
   default     = ["0.0.0.0/0"] # セキュリティのため、運用時は特定のIPに制限することを推奨
 }
