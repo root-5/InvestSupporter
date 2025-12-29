f35d5e705fcdf1f3efa4d8f4e82113915f52ebb8
root-5 <hujisannrokuoumunaku.shigoto@gmail.com>
2025-12-29 09:23:47 +0900

    main.tf の内容理解

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,21 +1,26 @@
+# Terraform はエントリーポイントが言語的には存在しない (.tf は設定ファイル群として扱われる) 
+# ただし、慣例的に main.tf がメインファイルとして扱われることが多い
+# main.tf だけで完結させてもよいが、役割ごとにファイルをモジュールに分割することが多い
 terraform {
-  # Terraform のバージョン指定、.terraform-version も同様に設定
+  # Terraform 本体のバージョン指定、.terraform-version も同様に設定
   required_version = ">= 1.14.3"
 
-  # プロバイダー指定、「拡張機能」に近い概念で各クラウドの API を操作するための宣言
+  # プロバイダー指定、「拡張機能」に近い概念、各クラウドの API を操作するための宣言
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0"
+      version = "~> 7.14.1"
     }
   }
 
-  # GCS Backend configuration
-  # バケット名はユニークである必要があります。作成したバケット名に書き換えてください。
-  # backend "gcs" {
-  #   bucket  = "invest-supporter-tfstate"
-  #   prefix  = "terraform/state"
-  # }
+  # 状態管理 (tfstate) の保存先設定
+  # バケット名はプロジェクト外を含めてユニークである必要あり
+  # インフラ作成 (terraform apply) 時点では存在している必要があるため、事前に手動で作成しておくこと
+  # パブリックアクセス不可かつバージョニング有効化を強く推奨
+  backend "gcs" {
+    bucket  = "invest-supporter"
+    prefix  = "terraform/state"
+  }
 }
 
 # プロバイダーの設定情報
