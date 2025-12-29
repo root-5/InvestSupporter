920920d954c1197eb331ccdf93204d6a40ed4b03
root-5 <hujisannrokuoumunaku.shigoto@gmail.com>
2025-12-30 03:12:00 +0900

    スタートアップスクリプトを修正

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -35,7 +35,7 @@ resource "google_compute_instance" "app_server" {
     curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     apt-get update
-    apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
     # ユーザーを docker グループに追加、sudo なしで docker コマンドを実行可能にする
     groupadd docker
     usermod -aG docker $USER
