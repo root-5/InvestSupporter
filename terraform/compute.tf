7a1505226383a85e709cd0ee5a9a81fc672a3cb4
root-5 <hujisannrokuoumunaku.shigoto@gmail.com>
2026-01-01 03:39:54 +0900

    GitHub Copilot の修正提案受け入れ

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -31,7 +31,6 @@ resource "google_compute_instance" "app_server" {
     # https://matsuand.github.io/docs.docker.jp.onthefly/engine/install/debian/#install-using-the-repository
     apt-get update
     apt-get install -y ca-certificates curl gnupg lsb-release
-    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     apt-get update
