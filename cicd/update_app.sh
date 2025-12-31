e0c554faff9fba5f9536f1aa6aa25cfd1d28cc02
root-5 <hujisannrokuoumunaku.shigoto@gmail.com>
2025-12-31 16:15:45 +0900

    CICD最終テスト

--- cicd/update_app.sh
+++ cicd/update_app.sh
@@ -8,14 +8,6 @@ APP_DIR="$HOME/InvestSupporter"
 
 cd "$APP_DIR"
 
-# 最新の main を取得
-git fetch origin main
-git reset --hard origin/main
-
-# テストのためここで終了
-touch $APP_DIR/ci_cd_compleeeeeeeeeeeeted.txt
-exit 0
-
 # アプリをビルド・再起動
 docker compose down -v
 docker rmi -f investsupporter-app # app のみ更新する場合
